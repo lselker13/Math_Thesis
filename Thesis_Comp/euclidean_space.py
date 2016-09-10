6a0417e6f4287ce32336864d3ac846f642a07ef0
Leo Selker <lselker13@gmail.com>
2016-09-09 21:12:03 -0700

    Bug fix

--- Thesis_Comp/euclidean_space.py
+++ Thesis_Comp/euclidean_space.py
@@ -7,24 +7,26 @@ import math
 
 
 class EuclideanSpace(object):
-    EPSILON = 10**8  # No real reason...
+    EPSILON = 10**(-8)  # No real reason...
 
     def __init__(self, data_array, scale_factor=1):
         # Data should be row major - each row is a point. Should be a numpy array.
-        data_array = data_array * scale_factor
+        data_array *= scale_factor
         print data_array
         (self.n_points, self.dimension) = data_array.shape
 
         # Calculate the distance matrix (using scipy)
         distance_matrix_compact = dist.pdist(data_array, 'euclidean')
         distance_matrix = dist.squareform(distance_matrix_compact)
+
         # Go through and exponentiate the distances
         vector_inverse_exp = np.vectorize(lambda n: math.exp(-n))
         self.distance_matrix = vector_inverse_exp(distance_matrix)
 
-
     def magnitude(self):
-        if linalg.cond(self.distance_matrix) < 1 / self.EPSILON or True:
+        # Can't get condition number to work... but I think it uses svd anyway
+        singular_values = linalg.svd(self.distance_matrix, compute_uv=False)
+        if singular_values[0] / singular_values[-1] < 1 / self.EPSILON:
             mobius_inversion = linalg.inv(self.distance_matrix)
             return np.sum(mobius_inversion)
         else:
@@ -34,6 +36,6 @@ if __name__ == '__main__':
     test_array = np.array(
         [[0, 0],
          [0, 2],
-         [3, 2]]
+         [0, 4]]
     )
     print EuclideanSpace(test_array, scale_factor=10).magnitude()
