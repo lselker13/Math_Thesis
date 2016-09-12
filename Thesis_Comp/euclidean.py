722301a66c36792031eeabc5e53c1894827513be
Leo Selker <lselker13@gmail.com>
2016-09-12 16:56:48 -0700

    Added multiscale magnitude

--- Thesis_Comp/euclidean.py
+++ Thesis_Comp/euclidean.py
@@ -12,26 +12,30 @@ class EuclideanSpace(object):
 
     def __init__(self, data_array, scale_factor=1):
         # Data should be row major - each row is a point. Should be a numpy array.
+        # We multiply early by the scale factor to give the option of permanently scaling the space
         data_array *= scale_factor
-        print data_array
         (self.n_points, self.dimension) = data_array.shape
 
         # Calculate the distance matrix (using scipy)
         distance_matrix_compact = dist.pdist(data_array, 'euclidean')
-        distance_matrix = dist.squareform(distance_matrix_compact)
+        self.distance_matrix = dist.squareform(distance_matrix_compact)
 
-        # Go through and exponentiate the distances
+    def magnitude(self, scale_factor=1):
+
+        # Scale then exponentiate the distances
         vector_inverse_exp = np.vectorize(lambda n: math.exp(-n))
-        self.distance_matrix = vector_inverse_exp(distance_matrix)
+        exp_distance_matrix = vector_inverse_exp(self.distance_matrix * scale_factor)
 
-    def magnitude(self):
-        # Can't get condition number to work... but I think it uses svd anyway
         singular_values = linalg.svd(self.distance_matrix, compute_uv=False)
         if singular_values[-1] / singular_values[0] > self.EPSILON:  # Check for singular matrix
-            mobius_inversion = linalg.inv(self.distance_matrix)
+            mobius_inversion = linalg.inv(exp_distance_matrix)
             return np.sum(mobius_inversion)
         else:
             return None
+
+    def multiscale_magnitude(self, scale_factor_iterable):
+        magnitude_list = map(self.magnitude, scale_factor_iterable)
+        return magnitude_list
 
 if __name__ == '__main__':
     test_array = np.array(
@@ -39,4 +43,5 @@ if __name__ == '__main__':
          [0, 2],
          [0, 4]]
     )
-    print EuclideanSpace(test_array, scale_factor=10).magnitude()
+    euclidean_space = EuclideanSpace(test_array)
+    print(euclidean_space.multiscale_magnitude((2, 3)))
